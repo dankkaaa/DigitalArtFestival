dbd97200b219d31883c22af5486a9bd57f6a5bc9
etozhedankkaaa <krip7117@gmail.com>
2026-03-04 02:24:29 +0300

    Initial commit: Digital Art Festival

--- config/settings.py
+++ config/settings.py
@@ -120,9 +120,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "/static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']   # если статика лежит в корневой папке static
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Media uploads (user content)
 MEDIA_URL = "/media/"
